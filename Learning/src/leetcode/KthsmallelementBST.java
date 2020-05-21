873772cddc8e820414fe9874abe3ae91f9715c59
Shubham Sharma <shubham1210@users.noreply.github.com>
2020-05-21 10:43:58 +0530

    updating with easy approach

--- Learning/src/leetcode/KthsmallelementBST.java
+++ Learning/src/leetcode/KthsmallelementBST.java
@@ -3,11 +3,14 @@ package leetcode;
 public class KthsmallelementBST {
     int count=0;
     public int kthSmallest(TreeNode root, int k) {
-        TreeNode node = traverse(root, k);
+        /*TreeNode node = traverse(root, k);
+        return node.val;*/
 
-        return node.val;
+        int[] num = new int[2];
+        traverseEasy(root,k,num);
+        return num[1];
     }
-
+    //inorder traversal give elemenet in asc order in BST
     public TreeNode traverse(TreeNode root, int k)
     {
         if(root==null) return null;
@@ -23,6 +26,20 @@ public class KthsmallelementBST {
 
     }
 
+    /*/
+    this is really easy to understand.
+     */
+    public void traverseEasy(TreeNode root, int k,int[] num) {
+        if (root == null) return;
+        traverseEasy(root.left, k, num);
+        if (++num[0] == k)
+        {
+            num[1] =root.val;
+            return;
+        }
+        traverseEasy(root.right,k,num);
+
+    }
     public static void main(String[] args) {
         //TreeNode root = new TreeNode(3,new TreeNode(1,null,new TreeNode(2,null,null)),new TreeNode(4));
         TreeNode root = new TreeNode(5,new TreeNode(3,new TreeNode(2,new TreeNode(1),null),new TreeNode(4,null,null)),new TreeNode(6));
