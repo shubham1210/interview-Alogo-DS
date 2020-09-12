9e498d32b7818293a4da4f85545d5450aa8e1557
Shubham Sharma <shubham1210@users.noreply.github.com>
2020-09-12 19:56:59 +0530

    Update Sort.java

--- Learning/src/sorting/Sort.java
+++ Learning/src/sorting/Sort.java
@@ -14,11 +14,30 @@ public class Sort {
         selectionSort(input);
         bubbleSort(input);
         quickSort(input,0,input.length-1);
+        insertionSort(intput);
         for(int i=0;i<input.length;i++)
         {
             System.out.print(input[i] + " ");
         }
     }
+     public void insertionSort(int arr[]) 
+        { 
+            int n = arr.length; 
+            for (int i = 1; i < n; ++i) { 
+                int key = arr[i]; 
+                int j = i - 1; 
+
+                /* Move elements of arr[0..i-1], that are 
+                   greater than key, to one position ahead 
+                   of their current position */
+                // whatevet is greter than i will move to next postion and then place key
+                while (j >= 0 && arr[j] > key) { 
+                    arr[j + 1] = arr[j]; 
+                    j = j - 1; 
+                } 
+                arr[j + 1] = key; 
+            } 
+        } 
 
     public static void bubbleSort(int[] array)
     {
